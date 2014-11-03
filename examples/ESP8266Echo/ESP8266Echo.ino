33fe82d6362ec966cd7375b1531bb97f4b1453c6
SoyNerdito <soynerdito@gmail.com>
2014-11-02 22:14:45 -0400

    Using a level shifter it works, without never!

---
+++ examples/ESP8266Echo/ESP8266Echo.ino
@@ -0,0 +1,61 @@
+/*
+  Software serial multple serial test
+ 
+ Receives from the hardware serial, sends to software serial.
+ Receives from software serial, sends to hardware serial.
+ 
+ The circuit: 
+ * RX is digital pin 10 (connect to TX of other device)
+ * TX is digital pin 11 (connect to RX of other device)
+ 
+ Note:
+ Not all pins on the Mega and Mega 2560 support change interrupts, 
+ so only the following can be used for RX: 
+ 10, 11, 12, 13, 50, 51, 52, 53, 62, 63, 64, 65, 66, 67, 68, 69
+ 
+ Not all pins on the Leonardo support change interrupts, 
+ so only the following can be used for RX: 
+ 8, 9, 10, 11, 14 (MISO), 15 (SCK), 16 (MOSI).
+ 
+ created back in the mists of time
+ modified 25 May 2012
+ by Tom Igoe
+ based on Mikal Hart's example
+ 
+ This example code is in the public domain.
+ 
+ */
+#include <SoftwareSerial.h>
+
+SoftwareSerial wifiPort(10, 11); // RX, TX
+
+void setup()  
+{
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+
+  // set the data rate for the SoftwareSerial port
+  wifiPort.begin(9600);
+  wifiPort.println("AT+RST");
+  delay(1000);
+  wifiPort.println("AT");
+}
+
+void loop() // run over and over
+{
+
+  wifiPort.listen();
+  if (wifiPort.available() ) {
+    Serial.write(wifiPort.read());
+
+  }
+
+  if (Serial.available() ){
+    wifiPort.write(Serial.read());
+  }
+
+}
+
